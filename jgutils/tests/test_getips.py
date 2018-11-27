c487f6ba6f63ca8b004b0089516f7a1af8a3ddcf
Jerod Gawne <jerodgawne@gmail.com>
2018-11-27 02:59:42 -0800

    add test_getips.py

---
+++ jgutils/tests/test_getips.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.7
+# coding=utf-8
+"""Test Get IPs: Jerod Gawne, 2018.11.27 <https://github.com/jerodg/jgutils>"""
+import logging
+import sys
+import traceback
+from unittest import TestCase
+
+from jgutils import getips
+
+logger = logging.getLogger(__name__)
+DBG = logger.isEnabledFor(logging.DEBUG)
+NFO = logger.isEnabledFor(logging.INFO)
+
+
+class TestGetIps(TestCase):
+    """TestGetIps"""
+
+    def test_ips(self):
+        with getips.GetIps() as gi:
+            self.assertTrue(gi.ips['lan4'] is not None)
+
+
+if __name__ == '__main__':
+    try:
+        print(__doc__)
+    except Exception as excp:
+        logger.exception(traceback.print_exception(*sys.exc_info()))
